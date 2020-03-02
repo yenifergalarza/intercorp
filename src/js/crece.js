32084c8c6b9c00c16cb42680e4073aa8c1813f71
unknown <yeniferalexandra2000@hotmail.com>
2020-03-02 16:47:18 -0500

    subiendo cambios en crece

--- src/js/crece.js
+++ src/js/crece.js
@@ -1,15 +1,31 @@
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
+const selected1 = document.querySelector(".selected");
+const optionsContainer1 = document.querySelector(".options-container");
 
 const optionsList = document.querySelectorAll(".option");
 
-selected.addEventListener("click", () => {
-    optionsContainer.classList.toggle("active");
+selected1.addEventListener("click", () => {
+    optionsContainer1.classList.toggle("active");
 });
 
 optionsList.forEach(o => {
     o.addEventListener("click", () => {
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("active");
+        selected1.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer1.classList.remove("active");
+    });
+});
+
+
+const selected2 = document.querySelector(".s2");
+const optionsContainer2 = document.querySelector(".oc2");
+const optionsList2 = document.querySelectorAll(".o2");
+
+selected2.addEventListener("click", () => {
+    optionsContainer2.classList.toggle("active");
+});
+
+optionsList2.forEach(o => {
+    o.addEventListener("click", () => {
+        selected2.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer2.classList.remove("active");
     });
 });
