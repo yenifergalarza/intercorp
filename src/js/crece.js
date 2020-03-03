91af2fd0e3afd270589a4738166e41f3fd319c1b
unknown <yeniferalexandra2000@hotmail.com>
2020-03-02 22:03:59 -0500

    cambios en metaetiquetas y crece .html

--- src/js/crece.js
+++ src/js/crece.js
@@ -1,7 +1,24 @@
-const selected1 = document.querySelector(".selected");
-const optionsContainer1 = document.querySelector(".options-container");
+// const selected1 = document.querySelector(".selected");
+// const optionsContainer1 = document.querySelector(".options-container");
 
-const optionsList = document.querySelectorAll(".option");
+// const optionsList = document.querySelectorAll(".option");
+
+// selected1.addEventListener("click", () => {
+//     optionsContainer1.classList.toggle("active");
+// });
+
+// optionsList.forEach(o => {
+//     o.addEventListener("click", () => {
+//         selected1.innerHTML = o.querySelector("label").innerHTML;
+//         optionsContainer1.classList.remove("active");
+//     });
+// });
+
+
+const selected1 = document.querySelector(".s1");
+const optionsContainer1 = document.querySelector(".oc1");
+
+const optionsList = document.querySelectorAll(".o1");
 
 selected1.addEventListener("click", () => {
     optionsContainer1.classList.toggle("active");
@@ -13,7 +30,6 @@ optionsList.forEach(o => {
         optionsContainer1.classList.remove("active");
     });
 });
-
 
 const selected2 = document.querySelector(".s2");
 const optionsContainer2 = document.querySelector(".oc2");
@@ -29,3 +45,61 @@ optionsList2.forEach(o => {
         optionsContainer2.classList.remove("active");
     });
 });
+
+
+
+const selected3 = document.querySelector(".s3");
+const optionsContainer3 = document.querySelector(".oc3");
+const optionsList3 = document.querySelectorAll(".o3");
+
+selected3.addEventListener("click", () => {
+    optionsContainer3.classList.toggle("active");
+});
+
+optionsList3.forEach(o => {
+    o.addEventListener("click", () => {
+        selected3.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer3.classList.remove("active");
+    });
+});
+
+
+
+
+const selected4 = document.querySelector(".s4");
+const optionsContainer4 = document.querySelector(".oc4");
+const optionsList4 = document.querySelectorAll(".o4");
+
+selected4.addEventListener("click", () => {
+    optionsContainer4.classList.toggle("active");
+});
+
+optionsList4.forEach(o => {
+    o.addEventListener("click", () => {
+        selected4.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer4.classList.remove("active");
+    });
+});
+
+
+const jobs = [{
+        id: 1,
+        title: "Product Manager",
+        level: "Especialista",
+        area: "Contabilidad",
+        modalidad: "tiempo completo",
+        time: "00/00/0000",
+        company: "promart"
+    },
+    {
+        id: 1,
+        title: "Product Manager",
+        level: "Trainee",
+        area: "Contabilidad",
+        modalidad: "tiempo completo",
+        time: "00/00/0000",
+        company: "promart"
+    }
+
+
+]
