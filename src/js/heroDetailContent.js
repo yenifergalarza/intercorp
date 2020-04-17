632e556773c3aa23ed4db81ff388e77159a1afcc
unknown <yeniferalexandra2000@hotmail.com>
2020-04-17 15:59:00 -0500

    offsetHeight por client height

--- src/js/heroDetailContent.js
+++ src/js/heroDetailContent.js
@@ -1,12 +1,12 @@
 const contentGrowth = document.getElementById("contentDetail");
 const container = document.getElementById("allContainerBackground");
 
-const contentGrowthHeight = contentGrowth.clientHeight;
+const contentGrowthHeight = contentGrowth.offsetHeight;
 
 
 if (window.innerWidth < 768) {
     console.log(contentGrowthHeight);
-    console.log(container.clientHeight);
-    container.setAttribute('style', `height: calc(${contentGrowthHeight}px + 15vh) !important`);
 
+    container.setAttribute('style', `height: calc(${contentGrowthHeight}px + 15vh) !important`);
+    console.log(container.clientHeight);
 }
